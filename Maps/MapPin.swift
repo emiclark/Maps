aa45afffae916a2ae6e4ce1e97395727e3b5fc51
emi clark <emiko.clark@gmail.com>
2016-04-30 17:54:15 -0400

    map+annotations+webpagedisplay:OK

--- Maps/MapPin.swift
+++ Maps/MapPin.swift
@@ -12,22 +12,22 @@ import MapKit
 class MapPin: NSObject, MKAnnotation {
 
     var title: String?
-    var subtitle: String?
+    var cuisine: String?
+    var summary: String?
+    var image: String?
     var locationName: String?
     var coordinate: CLLocationCoordinate2D
 
-    init( title: String, subtitle: String, locationName: String, coordinate: CLLocationCoordinate2D ) {
+    init( title: String, cuisine: String, summary: String, image: String, locationName: String,  coordinate: CLLocationCoordinate2D) {
 
         self.title = title
-        self.subtitle = subtitle
+        self.cuisine = cuisine
+        self.summary = summary
+        self.image = image
         self.locationName = locationName
         self.coordinate = coordinate
 
         super.init()
     }
-    
-//    var subtitle: String {
-//            return locationName
-//    }
     
 }
--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -8,14 +8,15 @@
 
 import UIKit
 import MapKit
-import CoreLocation 
+import CoreLocation
 
 class ViewController: UIViewController {
     let initialLocation = CLLocation(latitude: 40.741434, longitude: -73.990039)
-    let regionRadius: CLLocationDistance = 125
+    let regionRadius: CLLocationDistance = 300
     
     let locationManager: CLLocationManager = CLLocationManager()
-    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let authorizationStatus = CLLocationManager.authorizationStatus()    
+    
     
     @IBOutlet weak var selectedMapType: UISegmentedControl!
     @IBOutlet weak var mapView: MKMapView!
@@ -32,9 +33,15 @@ class ViewController: UIViewController {
             locationManager.requestWhenInUseAuthorization()
         }
         
-        // show pin for turntotech on map
+       
+       //initialize with restaurant data in flatiron district
+       flatIronRestaurantsData()
+       
+        // Add/Show pin for turntotech on map
         let turntotechPin = MapPin(title: "TurnToTech",
-            subtitle: "IOS BootCamp",
+            cuisine: "",
+            summary: "IOS BootCamp",
+            image: "logo.png",
             locationName: "184 5th Ave., NY, NY 10010",
             coordinate: CLLocationCoordinate2D(latitude: 40.741434, longitude: -73.990039))
         
@@ -68,40 +75,38 @@ class ViewController: UIViewController {
             mapView.mapType = MKMapType.Satellite
         }
         
+        
     }
     
     //MARK: Delegate Function
     // Delegate method called each time an annotation appears in the visible window
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        
+
+
         if annotation is MKUserLocation {
             //return nil so map view draws "blue dot" for standard user location
             return nil
         }
         
         let reuseId = "pin"
-        
-
         var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
         
         if pinView == nil {
             pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
             pinView!.canShowCallout = true
 
-            pinView?.image = UIImage(named: "flower.jpg")
-            let imageview = UIImageView(frame: CGRectMake(0, 0, 45, 45))
-            imageview.image = UIImage(named: "logo.png")
 
-//            // Add image to left callout
-//            var mugIconView = UIImageView (image: UIImage(named: "apple.png"))
-//            pinView!.leftCalloutAccessoryView = mugIconView
-//            
-//            // Add image to right callout
-//            var myRrightCalloutAccessoryView = UIButton(type: .DetailDisclosure)as UIButton
-//            pinView!.rightCalloutAccessoryView = myRightCalloutAccessoryView
+
+            var newImgThumb : UIImageView
+            newImgThumb = UIImageView(frame:CGRectMake(0, 0, 100, 70))
+            newImgThumb.contentMode = .ScaleAspectFit
             
-        } else {
-            pinView!.annotation = annotation
+
+            // Left Accessory
+            let leftAccessory = UILabel(frame: CGRectMake(0,0,50,30))
+            leftAccessory.text = description
+            leftAccessory.font = UIFont(name: "Verdana", size: 10)
+            pinView!.leftCalloutAccessoryView = leftAccessory
         }
         return pinView
     }
@@ -114,5 +119,52 @@ class ViewController: UIViewController {
 //    }
 
 
+    //load initial restaurant data for flatiron district
+    func flatIronRestaurantsData() {
+        // Add/Show pin for restaurants in Flatiron District
+        let gramercyTavern = MapPin(title: "Gramercy Tavern",
+            cuisine: "American",
+            summary: "Danny Meyer's Flatiron District tavern with a fixed-price-only dining room & a bustling bar area.",
+            image: "gramercy.png",
+            locationName: "42 E 20th St.",
+            coordinate: CLLocationCoordinate2D(latitude: 40.738541, longitude: -73.988504))
+        mapView.addAnnotation(gramercyTavern)
+        
+        let giorgios = MapPin(title: "Giorgio's of Gramercy",
+            cuisine: "American",
+            summary: "Longtime eatery serving a New American menu along with numerous pasta dishes.",
+            image: "gorgios.png",
+            locationName: "27 E 21st St.",
+            coordinate: CLLocationCoordinate2D(latitude: 40.739716, longitude: -73.988622))
+        mapView.addAnnotation(giorgios)
+        
+        let elevenMadisonPark = MapPin(title: "Eleven Madison Park",
+            cuisine: "American",
+            summary: "Upscale American tasting menus from chef Daniel Humm served in a high-ceilinged art deco space.",
+            image: "eleven.png",
+            locationName: "11 Madison Ave.",
+            coordinate: CLLocationCoordinate2D(latitude: 40.73748, longitude: -73.981331))
+        mapView.addAnnotation(elevenMadisonPark)
+        
+        let tacombi = MapPin(title: "Tacombi Flatiron",
+            cuisine: "Mexican",
+            summary: "Hopping market-feeling eatery comprising a taqueria, juice stand & Veracruz-style coffee bar.",
+            image: "tacombi.png",
+            locationName: "30 W 24th St.",
+            coordinate: CLLocationCoordinate2D(latitude: 40.7425626, longitude: -73.9909777))
+        mapView.addAnnotation(tacombi)
+        
+        let ILILI = MapPin(title: "ILILI",
+            cuisine: "Mediterranean",
+            summary: "Creative Lebanese small plates ideal for sharing in a trendy space that also serves cocktails.",
+            image: "gramercy.png",
+            locationName: "236 5th Ave.",
+            coordinate: CLLocationCoordinate2D(latitude: 40.748379, longitude: -73.996712))
+        mapView.addAnnotation(ILILI)
+        
+    }
+
+
 }
+
 
